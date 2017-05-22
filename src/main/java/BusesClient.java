6b225858985fb9d28c9a0c25ebfff7710ea34187
Danny6216 <dann6216@gmail.com>
2017-05-22 20:55:51 +0300

    initial project version

---
+++ src/main/java/BusesClient.java
@@ -0,0 +1,23 @@
+/**
+ * Created by dann on 20.05.17.
+ */
+import org.apache.log4j.Logger;
+import org.junit.Test;
+public class BusesClient {
+    private static Logger logger = Logger.getLogger(BusesClient.class.getName());
+    private void logBasicSystemInfo() {
+        logger.info("Launching the application...");
+        logger.info("Operating System: " + System.getProperty("os.name") + " "+ System.getProperty("os.version"));logger.info("JRE: " + System.getProperty("java.version"));
+        logger.info("Java Launched From: " + System.getProperty("java.home"));
+        logger.info("Class Path: " + System.getProperty("java.class.path"));
+        logger.info("Library Path: " + System.getProperty("java.library.path"));
+        logger.info("User Home Directory: " + System.getProperty("user.home"));
+        logger.info("User Working Directory: " + System.getProperty("user.dir"));
+        logger.info("Test INFO logging.");
+    }
+    public BusesClient(){
+        logger.debug("TransportClient[0]: starting application.........");
+        logger.debug ("sdfsdfs");
+        logger.debug("");
+    }
+}
